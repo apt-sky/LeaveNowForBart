26b2bfa8a0a08a255c59d306b37c71e9f9cc28cf
Aakash <athakkar@salesforce.com>
2017-03-05 20:21:34 -0800

    Breaking into functions and formatting

--- server.js
+++ server.js
@@ -7,35 +7,45 @@ var parseString = require('xml2js').parseString;
 var host = 'http://api.bart.gov/api/etd.aspx?cmd=etd&orig=EMBR&key=MW9S-E7SL-26DU-VV8V'
 
 function getEtd() {
-    request(host, function (error, response, body){
-        if (error) {
-            console.log('error:', error);
-        } 
-        else if (response && response.statusCode == 200) {
-            parseString (body, function(err, result) {
-                if (err) {
-                    console.log('Error while parsing response')        
-                } else {
-                    var destinations = result.root.station.etd;
-                    var stationEmbr = result.root.station[0];
-                    var stationEmbrName = stationEmbr.name;
-                    var stationEmbrEtd = stationEmbr.etd;
-
-                    stationEmbrEtd.forEach(function(etdItem){
-                        if(etdItem.abbreviation[0] === 'MLBR') {
-                            var estimates = etdItem.estimate;
-                            estimates.forEach(function(estimatesItem){
-                                console.log(estimatesItem.minutes[0]);
-                            });
-                        }
-                    });
+        request(host, function (error, response, body){
+                if (error) {
+                        console.log('error:', error);
+                } 
+                else if (response && response.statusCode == 200) {
+                        parseString (body, function(err, result) {
+                                if (err) {
+                                        console.log('Error while parsing response');
+                                } else {        
+                                        getEtdsForStation(result.root.station[0]);
+                                }
+                        });
                 }
-            });
+                 else {
+                        console.log('Sorry Couldn\'t Retrieve status');
+                 }
+        });
+};
+
+function getEtdsForStation(station) {
+        var stationName = station.name;
+        var stationEtd = station.etd;
+
+        stationEtd.forEach(function(etdItem){
+                printLeavingTimes(etdItem, 'MLBR');
+        });
+ };
+
+function printLeavingTimes(etdItem, destinationAbbrv) {
+        if (etdItem.abbreviation[0] === destinationAbbrv) {
+                var estimates = etdItem.estimate;
+                var destination = etdItem.destination[0];
+                console.log("The Next Trains to " + destination + " are leaving in ...");
+                estimates.forEach(function(estimatesItem) {
+                        var minutes = estimatesItem.minutes[0];
+                        console.log(minutes + " minutes");
+                });
+                console.log();
         }
-        else {
-            console.log('Sorry Couldn\'t Retrieve status')
-        }
-    });
 };
 
 const server = http.createServer((req, res) => {
