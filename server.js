89497b9523c6cf91bd0c1ad80f9f0054b493513f
Aakash <athakkar@salesforce.com>
2017-03-07 00:50:48 -0800

    Adding callbacks and some error case handling

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var sourceCode = 'EMBR';
 var destinationCode = 'MLBR';
 var host = 'http://api.bart.gov/api/etd.aspx?cmd=etd&orig='+sourceCode+'&key=MW9S-E7SL-26DU-VV8V'
 
-function getEtd(res) {
+function getEtd(callback) {
         request(host, function (error, response, body){
                 if (error) {
                         console.log('error:', error);
@@ -17,13 +17,15 @@ function getEtd(res) {
                                 if (err) {
                                         console.log('Error while parsing response');
                                 } else {        
+                                        var minutes = []
                                         var sourceStation = result.root.station[0];
                                         var destinationEtdItem = getDestinationEtdForSourceStation(sourceStation);
-                                        var minutes = getMinutesForEtd(destinationEtdItem);
-                                        printLeavingTimesToConsole(minutes, destinationEtdItem.destination[0]);
-                                        if (res) {
-                                                res.send(minutes);
+                                        if (destinationEtdItem) {
+                                                minutes = getMinutesForEtd(destinationEtdItem);
+                                                printLeavingTimesToConsole(minutes, destinationEtdItem.destination[0]);
                                         }
+                                        console.log("Returning minutes: " + minutes);
+                                        callback(minutes);
                                 }
                         });
                 }
@@ -43,6 +45,7 @@ function isEtdRequiredDestination(etdItem) {
 };
 
 function getMinutesForEtd(etdItem) {
+
         var estimates = etdItem.estimate;
         var minutes = [];
         estimates.forEach(function(estimatesItem) {
@@ -68,17 +71,23 @@ function printLeavingTimesToConsole(minutes, destination) {
 var app = express();
 
 app.get('/', function(req, res) {
-        getEtd(res);
+        console.log("Server called to get ETD");
+        getEtd(function(minutes){
+                console.log('Server Returned minutes: ' + minutes);
+                res.send(minutes);
+        });
 })
 
 app.listen(3000, function() {
-        console.log("LeaveNowForBart app listening on port 3000!");
+        console.log("LeaveNowForBart app listening on port 3000!\n");
 })
 
 var job = new CronJob('0 */7 * * * *', function(){
-        console.log('Run every 7 minutes')
+        console.log('CronJob running every 7 minutes to get ETD');
         console.log(new Date());
-        getEtd();
+        getEtd(function(minutes){
+                console.log("Server Returned minutes for CronJob: " + minutes);
+        });
 }, null, true, 'America/Los_Angeles');
 
 job.start();
